35a93046ce0479bd07365089b9cb8e806c95b57a
shubham7408 <shubhambhosale2360@gmail.com>
2024-11-25 15:10:02 +0530

    Added the Navbar profile and Cart

--- Front-end/src/components/Navbar.jsx
+++ Front-end/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import {assets} from "../assets/frontend_assets/assets"
-import { NavLink } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 const Navbar = () => {
   return (
@@ -28,8 +28,25 @@ const Navbar = () => {
 
         <div className="flex items-center gap-6 ">
             <img src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
-        </div>
 
+            <div className="group relative">
+                <img className="w-5 cursor-pointer" src={assets.profile_icon} alt="" />
+
+                <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+                    <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
+                        <p className="cursor-pointer hover:text-black">My Profile</p>
+                        <p className="cursor-pointer hover:text-black"> Orders </p>
+                        <p className="cursor-pointer hover:text-black">Logout</p>
+                    </div>
+                </div>
+            </div>
+
+            <Link to = "/cart" className="relative">
+            <img src={assets.cart_icon} className="w-5 min-w-5:" alt="cart" />
+            <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white "></p>
+            </Link>
+
+        </div>
     </div>
   )
 }
