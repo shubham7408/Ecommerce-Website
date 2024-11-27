e7597e9c7558915a9fb12e2e460f08b64a172453
shubham7408 <shubhambhosale2360@gmail.com>
2024-11-27 11:53:37 +0530

    Added the context api and fetch the data

--- Front-end/src/components/LatestCollections.jsx
+++ Front-end/src/components/LatestCollections.jsx
@@ -1,20 +1,35 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from "../context/ShopContext"
+import Title from "./Title"
+import ProductItem from './ProductItem';
 
 const LatestCollections = () => {
 
     const { products } = useContext(ShopContext);
+    const [LatestProducts,SetLatestProducts] = useState([]);
 
-    console.log(products);
-
+    useEffect(() => {
+      SetLatestProducts(products.slice(0,10));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[])
     
-  return (
-    <div>
-
-
-
+    return (
+  <div className="my-10">
+    <div className= "text-center py-8 text-3xl">
+      <Title text1={"LATEST"} text2={"COLLECTION"}/>
+      <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-700">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed neque, aliquid obcaecati provident veritatis tempore expedita quos sapiente ea nesciunt.
+      </p>
     </div>
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+      {
+        LatestProducts.map((item,index) =>(
+          <ProductItem key={index} id={item.id} image={item.image} name={item.name} price={item.price}/>
+        ))
+      }
+    </div>
+  </div>
   )
 }
 
