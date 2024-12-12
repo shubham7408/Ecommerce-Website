194962442febc7f9d9667580b0883892058af212
shubham7408 <shubhambhosale2360@gmail.com>
2024-12-12 11:41:40 +0530

    Needs to fix the Product page and context api

--- Front-end/src/pages/Product.jsx
+++ Front-end/src/pages/Product.jsx
@@ -1,7 +1,58 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from "react-router-dom"
+import { ShopContext } from "../context/ShopContext";
+
 const Product = () => {
-  return (
-    <div>Product</div>
-  )
+
+  const {productId} = useParams();
+  const {products} = useContext(ShopContext);
+  const [productData,setProductData] = useState(false);
+  const [image,setImage] = useState("");
+
+
+  const fetchProductData = async () => {
+
+    products.map((item) => {
+      if (item._id === productId) {
+        setProductData(item)
+        setImage(item.image[0])
+        return null;
+      }
+    })
+
+  }
+
+  useEffect(() => {
+    fetchProductData();
+    
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[products,productId]);
+
+  return productData ? (
+    <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">
+    {/* Product Data */}
+    <div className="flex gap-12 sm:gap-12 flex-col sm:flex-row">
+      {/* Product Image */}
+
+      <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
+        <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
+          {
+            productData.image.map((item,index) =>(
+              <img src={item} key={index} className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer" alt="Product images" />
+            ))
+          }
+        </div>
+        <div className="w-full sm:w-[80%]">
+          <img src={image} alt="Product Images" />
+        </div>
+      </div>
+
+    </div>
+      Product
+    </div>
+  ) : <div className="opacity-0"></div>
 }
 
 export default Product
+
