d156811957e0208ada9ea90862cd0cc74ab395ed
shubham7408 <shubhambhosale2360@gmail.com>
2024-12-13 14:51:51 +0530

    Solved the context api error for not loading data

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
         <Route path = "/collections" element={<Collection/>}/>
         <Route path = "/about" element={<About/>}/>
         <Route path = "/contact" element={<Contact/>}/> 
-        <Route path = "/product/:productID" element={<Product/>}/>
+        <Route path = "/product/:productId" element={<Product/>}/>
         <Route path = "/cart" element={<Cart/>}/>
         <Route path = "/login" element={<Login/>}/>
         <Route path = "/placeorders" element={<PlaceOrders/>}/>
--- Front-end/src/pages/Product.jsx
+++ Front-end/src/pages/Product.jsx
@@ -1,12 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useContext, useEffect, useState } from 'react'
-import { useParams } from "react-router-dom"
-import { ShopContext } from "../context/ShopContext";
+import {useParams} from "react-router-dom"
+import {ShopContext} from "../context/ShopContext";
+import { assets } from '../assets/frontend_assets/assets';
 
 const Product = () => {
 
   const {productId} = useParams();
-  const {products} = useContext(ShopContext);
+  const {products,currency} = useContext(ShopContext);
   const [productData,setProductData] = useState(false);
   const [image,setImage] = useState("");
 
@@ -16,6 +17,7 @@ const Product = () => {
     products.map((item) => {
       if (item._id === productId) {
         setProductData(item)
+        console.log(item);
         setImage(item.image[0])
         return null;
       }
@@ -39,17 +41,39 @@ const Product = () => {
         <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
           {
             productData.image.map((item,index) =>(
-              <img src={item} key={index} className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer" alt="Product images" />
+              <img onClick={() => setImage(item)} src={item} key={index} className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer" alt="Product images" />
             ))
           }
         </div>
         <div className="w-full sm:w-[80%]">
-          <img src={image} alt="Product Images" />
+          <img className="w-full h-auto" src={image} alt="Product Images" />
         </div>
       </div>
 
+      {/* Product Info */}
+
+      <div className="flex-1">
+        <h1 className="font-medium text-2xl mt-2">{productData.name}</h1>
+        <div className="flex items-center gap-1 mt-2">
+          <img src={assets.star_icon} alt="Star icon" className="w-3 5" />
+          <img src={assets.star_icon} alt="star icon" className="w-3 5" />
+          <img src={assets.star_icon} alt="star icon" className="w-3 5" />
+          <img src={assets.star_icon} alt="star icon" className="w-3 5" />
+          <img src={assets.star_dull_icon} alt="star icon" className="w-3 5" />
+          <p className="pl-2">(122)</p>
+        </div>
+        <p className="mt-5 text-3xl font-medium">{currency}{productData.price}</p>
+        <p className="mt-5 text-gray-500 md:w-4/5">{productData.description}</p>
+        <div className="flex flex-col gap-4 my-8">
+          <p>Select Size</p>
+          <div className="flex gap-2">
+            {productData.sizes.map((item,index) => (
+              <button key={index}>{item}</button>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
-      Product
     </div>
   ) : <div className="opacity-0"></div>
 }
