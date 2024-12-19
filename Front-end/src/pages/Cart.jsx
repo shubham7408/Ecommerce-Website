4102fa8352eb7a437dcc9d0c0e81163856cfaa0e
shubham7408 <shubhambhosale2360@gmail.com>
2024-12-19 12:21:49 +0530

    Added the cart and context api bug fixes

--- Front-end/src/pages/Cart.jsx
+++ Front-end/src/pages/Cart.jsx
@@ -1,4 +1,28 @@
+import { useContext, useEffect, useState } from "react"
+import {ShopContext} from "../context/ShopContext";
 const Cart = () => {
+
+  const {products,currency,cartitems}= useContext(ShopContext);
+
+  const [cartData,setCartData] = useState([]);
+
+  useEffect(() => {
+    const tempData = [];
+    for(const items in cartitems){
+      for(const item in cartitems[items]){
+        if(cartitems[items][item] > 0){
+          tempData.push({
+            _id: items,
+            size:item,
+            quantity:cartitems[items][item]
+          })
+        }
+      }
+    }
+    console.log(tempData);
+
+  },[cartitems]);
+
   return (
     <div>Cart</div>
   )
