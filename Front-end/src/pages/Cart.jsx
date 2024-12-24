ac5ab42e64bc5337346f8b729fc53afa818051ad
shubham7408 <shubhambhosale2360@gmail.com>
2024-12-24 10:17:47 +0530

    Added the cart page

--- Front-end/src/pages/Cart.jsx
+++ Front-end/src/pages/Cart.jsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from "react"
 import {ShopContext} from "../context/ShopContext";
+import {Title} from "../components/Title";
 const Cart = () => {
 
   const {products,currency,cartitems}= useContext(ShopContext);
@@ -24,7 +25,31 @@ const Cart = () => {
   },[cartitems]);
 
   return (
-    <div>Cart</div>
+    <div className="border-t pt-14">
+      <div className="text-2xl mb-3">
+        <Title text1={"YOUR"} text2={"CART"}/>
+      </div>
+      <div>
+        {
+          cartData.map((item,index) => {
+            const productData = products.find((product) => product._id === item._id);
+
+            return(
+              <div key={index} className="py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4">
+                <div className="flex items-start gap-6">
+                  <img className="w-16 sm:w-20" src={productData.image[0]} alt="product images" />
+                  <div>
+                    <p className="text-xs sm:text-lg font-meduim">{productData.name}</p>
+                  </div>
+                </div>
+
+              </div>
+            )
+
+          })
+        }
+      </div>
+    </div>
   )
 }
 
