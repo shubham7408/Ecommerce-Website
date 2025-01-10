76806fd1e267ba60c83ff4ab8a7aebd379c9b177
shubham7408 <shubhambhosale2360@gmail.com>
2025-01-10 17:12:28 +0530

    Created the Admin Folder with CSS

---
+++ admin/src/App.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App;
